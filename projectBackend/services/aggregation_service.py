10244bad44f37601ddb152138e855989158c3949
Gigis09 <Gigiscamo09@gmail.com>
2026-04-01 21:56:03 -0400

    Enhance filter_companies with pagination and sorting
    Refactored the filter_companies method to include pagination and dynamic sorting.

--- projectBackend/services/aggregation_service.py
+++ projectBackend/services/aggregation_service.py
@@ -2,32 +2,35 @@ from sqlalchemy import text
 from app.core.db import SessionLocal
 
 class AggregationService:
+ @staticmethod
+def filter_companies(industry=None, year=None, min_spend=None,page=1, limit=10, sort_by="total_spend", order="desc"):
 
-    @staticmethod
-    def filter_companies(industry=None, year=None, min_spend=None):
-        db = SessionLocal()
+db = SessionLocal()
 
-        query = text("""
-            SELECT c.name, SUM(f.amount) as total_spend
-            FROM companies c
-            JOIN filings f ON c.id = f.company_id
-            WHERE 1=1
-              AND (:industry IS NULL OR c.industry = :industry)
-              AND (:year IS NULL OR f.year = :year)
-            GROUP BY c.name
-            HAVING (:min_spend IS NULL OR SUM(f.amount) >= :min_spend)
-            ORDER BY total_spend DESC;
-        """)
+ offset = (page - 1) * limit
 
-        result = db.execute(query, {
-            "industry": industry,
-            "year": year,
-            "min_spend": min_spend
-        })
-
-        rows = result.fetchall()
-
-        return {
-            "results": [dict(row) for row in rows],
-            "count": len(rows)
-        }
+query = text(f"""
+SELECT c.name, SUM(f.amount) as total_spend
+FROM companies c
+JOIN filings f ON c.id = f.company_id
+WHERE 1=1
+AND (:industry IS NULL OR c.industry = :industry)
+AND (:year IS NULL OR f.year = :year)
+GROUP BY c.name
+HAVING (:min_spend IS NULL OR SUM(f.amount) >= :min_spend)
+ORDER BY {sort_by} {order}
+LIMIT :limit OFFSET :offset; """)
+result = db.execute(query, { 
+        "industry": industry,
+        "year": year,
+        "min_spend": min_spend,
+        "limit": limit,
+        "offset": offset
+    })
+rows = result.fetchall()
+return {
+        "results": [dict(row) for row in rows],
+        "page": page,
+        "limit": limit,
+        "count": len(rows)
+    }
