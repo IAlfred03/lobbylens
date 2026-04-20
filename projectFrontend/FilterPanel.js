7147ef90e8d7ab06793b1355827f5bf4176117ce
Gigis09 <Gigiscamo09@gmail.com>
2026-04-19 20:59:49 -0400

    Implement FilterPanel component
    Add FilterPanel component with year and min spend inputs

--- projectFrontend/FilterPanel.js
+++ projectFrontend/FilterPanel.js
@@ -1,1 +1,31 @@
+import React from "react";
+export default function FilterPanel({ filters, setFilters }) {
+  return (
+    <div>
+      <h3>Filters</h3>
 
+      <input
+        placeholder="Year"
+        onChange={(e) =>
+          setFilters({ ...filters, year: e.target.value })
+        }
+      />
+
+      <input
+        placeholder="Min Spend"
+        onChange={(e) =>
+          setFilters({ ...filters, min_spend: e.target.value })
+        }
+      />
+
+      <select
+        onChange={(e) =>
+          setFilters({ ...filters, order: e.target.value })
+        }
+      >
+        <option value="desc">High → Low</option>
+        <option value="asc">Low → High</option>
+      </select>
+    </div>
+  );
+}
