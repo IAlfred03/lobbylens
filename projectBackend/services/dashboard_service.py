a86db707e9c52993b24c304169ee2cf71a4b064b
Gigis09 <Gigiscamo09@gmail.com>
2026-04-19 20:33:22 -0400

    Implement caching for dashboard summary
    Added caching to the dashboard summary to improve performance.

--- projectBackend/services/dashboard_service.py
+++ projectBackend/services/dashboard_service.py
@@ -1,11 +1,17 @@
 from sqlalchemy import text
 from app.core.db import SessionLocal
+from cachetools import  TTLCache
+
+cache = TTLCache(maxsize=100, ttl=300)
 
 class DashboardService:
 
     @staticmethod
     def get_summary():
-        db = SessionLocal()
+        if "dashboard" in cache:
+            return cache ["dashboard"]
+            
+    db = SessionLocal()
 
         top_companies = db.execute(text("""
             SELECT c.name, SUM(f.amount) as total_spend
@@ -16,6 +22,27 @@ class DashboardService:
             LIMIT 5
         """)).fetchall()
 
-        return {
-            "top_companies": [dict(row) for row in top_companies]
-        }
+        issues = db.execute(text("""
+            SELECT i.issue_name, SUM(f.amount) as total_spend
+            FROM issues i
+            JOIN filings f ON i.id = f.issue_id
+            GROUP BY i.issue_name
+            ORDER BY total_spend DESC
+            LIMIT 5
+        """)).fetchall()
+
+       result = {
+           "success" : True,
+           "top_companies": {
+               "labels": [c["name"] for c in companies],
+               "data": [c["total_spend"] for c in companies]
+           },
+           "top_issues": {
+               "labels": [i["issue_name"] for i in issues],
+               "data": [i["total_spend"] for i in issues]
+           }
+       }
+cache["dashboard"] = result
+return result
+
+    
